9de6c9382460e5039758a41d60343fbf158c0c76
Stefan Gräwe <stefan.graewe@gmail.com>
2019-04-21 11:25:08 +0200

    Add default lang attribute

--- template/nuxt/nuxt.config.js
+++ template/nuxt/nuxt.config.js
@@ -9,6 +9,7 @@ module.exports = {
   */
   head: {
     titleTemplate: '%s | ' + pkg.name,
+    htmlAttrs: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
